b4cbab8d02f389d722379ef75c08c03172f9b6d5
Nicole Rodriguez <nicolerodriguezcab@gmail.com>
2023-11-20 13:39:57 -0500

    ready for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
 import NavBar from "./Components/NavBar";
 import Index from "./Components/Index";
 import NewForm from "./Components/NewForm";
 import Show from "./Components/Show";
-import Edit from "./Components/EditForm";
-import "./app.css";
-import Home from "./Pages/Home";
+import EditForm from "./Components/EditForm";
+
 const App = () => {
   return (
     <Router>
@@ -17,7 +17,7 @@ const App = () => {
           <Route path="/all" element={<Index />} />
           <Route path="/new" element={<NewForm />} />
           <Route path="/show/:id" element={<Show />} />
-          <Route path="/edit/:id" element={<Edit />} />
+          <Route path="/edit/:id" element={<EditForm />} />
         </Routes>
       </div>
     </Router>
